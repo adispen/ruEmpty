efd5a8515e645daedc10f4c4e120dc08be35754a
Aedan Dispenza <aedan.dispenza@gmail.com>
2015-05-07 12:08:28 -0400

    added rooms page and func. shows table

--- app.py
+++ app.py
@@ -4,9 +4,13 @@ import requests
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def main_page():
 	buildings=['one', 'two', 'three', 'examples']
 	return render_template('index.html', buildings=buildings)
+
+@app.route('/rooms')
+def rooms_page():
+	return render_template('rooms.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
