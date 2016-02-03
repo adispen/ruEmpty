411838663d267ea63e2c16318dae72f17939f71d
Aedan Dispenza <aedan.dispenza@gmail.com>
2016-02-03 15:58:41 -0500

    committing for merge

--- app.py
+++ app.py
@@ -39,79 +39,120 @@ def linebreaksbr(eval_ctx, value):
 	paras = u'\n\n'.join(paras)
 	return Markup(paras) 
 
-class timeSlot:
-	def __init__(self, time):
+class roomClass:
+	def __init__(self, roomNum, time):
+		""" This class is used to define rooms using their number and the times
+		they are available given the date selected by the user.
+		"""
+		self.roomNum = roomNum
 		self.time = time
 
-class roomClass:
-	def __init__(self, roomNum, slot):
-		self.roomNum = roomNum
-		self.slot = slot
-
-def getBuildings():
-	campus = request.form.get("campus")
-	form = queryForm(request.POST)
-	if campus == "Busch":
-		form.building.choices = buildingsArr;
-
 class queryForm(Form):
-	day = SelectField(u'Day of the Week', coerce=str, choices=[("Monday", 'Monday'), ("Tuesday", 'Tuesday'), ("Wednesday", 'Wednesday'), ("Thursday", 'Thursday'), ("Friday", 'Friday')])
-	campus = SelectField(u'Campus', coerce=str, choices=[("Please choose a campus", 'Please choose a campus'), ("Busch", 'Busch'), ("Livingston", 'Livingston'), ("College Avenue", 'College Avenue'), ("Cook/Douglass", 'Cook/Douglass')])
-	building = SelectField(u'Building', coerce=str, choices=[("Please choose a campus first", 'Please choose a campus first')])
+	"""Defining form which will be rendered on the main page via WTForms."""
+	dayChoices = [("Monday", 'Monday'), ("Tuesday", 'Tuesday'), \
+			("Wednesday", 'Wednesday'), ("Thursday", 'Thursday'), \
+			("Friday", 'Friday')]
+
+	# Initial choice set to prompt user to change the campus, which fires the
+	# GET request which will get all buildings for that campus.
+	campusChoices = [("Please choose a campus", 'Please choose a campus'), \
+			("Busch", 'Busch'), ("College Avenue", 'College Avenue'), \
+			("Cook/Douglass", 'Cook/Douglass'), \
+			("Livingston", 'Livingston')]
+
+	# Initially set to a single option because it will be populated by the
+	# client based on the chosen campus.
+	defaultBuildings = [("Please choose a campus first", \
+			'Please choose a campus first')] 
+	day = SelectField(u'Day of the Week', coerce=str, choices=dayChoices)
+	campus = SelectField(u'Campus', coerce=str, choices=campusChoices)
+	building = SelectField(u'Building', coerce=str, choices=defaultBuildings)
 
 @app.route('/', methods=['GET', 'POST'])
 def main_page():
+	"""Creating a form from the defined class above, rendering it on the page.
+	"""
 	form = queryForm()
 	return render_template('index.html', form=form)
 
 @app.route('/rooms', methods=['GET', 'POST'])
 def rooms_page():
+	"""Displays the rooms and times they are open for specified day, campus,
+	and building.
+	"""
+
+	# Grabbing selected options from form on previous page.
 	dayOfWeek = request.form.get("day")
 	campusName = request.form.get("campus")
 	buildingName = request.form.get("building")
+
+	# If somehow they manage to submit with nothing selected catch as bug.
+	# TODO: Should really be handled by validators.
+	if campusName == "Please choose a campus" \
+			or buildingName == "Please choose a campus first":
+		return render_template('bug.html')
+
+	# Create an array to store rooms in.
 	rooms = []
-	timesArr = []
+
+	# Time to parse the JSON.
 	with open('ruemptyJSON.json') as data_file:
 		data = json.load(data_file)
-		for entry in data:
-			if entry["Campus Name"] == campusName:
-				for building in entry["Buildings"]:
-					if building["Building Name"] == buildingName:
-						for room in building["Rooms"]:
-							roomNum = room["Room Number"]
-							print "********* START" + roomNum + "*********** \n"
-							for day, times in room.iteritems():
-								if day == "Room Number":
-									continue
-								if day == dayOfWeek[:1]:
-									print dayOfWeek[:1]
-									print times
-									newTimeSlot = timeSlot(times)
-									timesArr.append(newTimeSlot)
+		i = 0
 
-							print len(timesArr)
-							newRoom = roomClass(roomNum, timesArr)
-							timesArr=[]
+		# These two filters make sure that we access the correct building
+		# object inside the correct campus object. From there we iterate
+		# through the rooms and grab what we need for each.
+		campus = filter(lambda entry: entry["Campus Name"] == campusName, data)
+		building = filter(lambda entry: entry["Building Name"] == buildingName\
+				, campus[0]["Buildings"])
+		for room in building[0]["Rooms"]:
+			roomNum = room["Room Number"]
 
-							rooms.append(newRoom)
-							print  "******** END" + roomNum + " **********  \n"
+			# Catch if the day is Thurday since evaluating the normal way would
+			# get us the schedule for Tuesday.
+			if dayOfWeek == "Thursday":
 
-	return render_template('rooms.html', dayOfWeek=dayOfWeek, campusName=campusName, buildingName=buildingName, rooms=rooms)
+				# Create new room objects of the times and number and add them
+				# to the array.
+				newRoom = roomClass(roomNum, room["TH"])
+				rooms.append(newRoom)
+				continue
+
+			newRoom = roomClass(roomNum, room[dayOfWeek[:1]])
+			rooms.append(newRoom)
+
+	# Returning the necessary data to render the rooms page correctly.
+	return render_template('rooms.html', dayOfWeek=dayOfWeek, \
+			campusName=campusName, buildingName=buildingName, rooms=rooms)
 
 @app.route('/_getBuildings', methods=['GET', 'POST'])
 def getBuildings():
+	"""The client will send a GET request to this route when
+	selecting a campus, and the server will return an alphabetized list of all
+	the available buildings on that campus.  The client uses this to populate
+	the selector for buildings.
+	"""
+
+	# Grabs the campus from the GET args.
 	campus = request.args['campus']
+
+	# Make an array in which top return our buildings.
 	buildingArr = []
+
+	# Once again accessing the data file and filtering to our desired campus.
 	with  open('ruemptyJSON.json') as data_file:
 		data = json.load(data_file)
-		for entry in data:
-			if entry["Campus Name"] == campus:
-				for buildingEntry in entry["Buildings"]:
-					buildingArr.append(buildingEntry["Building Name"])
+		campusData = filter(lambda entry: entry["Campus Name"] == campus, data)
 
-	print buildingArr
-	return jsonify(buildings=buildingArr)
+		# Populate the array with all buildings in that campus.
+		for building in campusData[0]["Buildings"]:
+			buildingArr.append(building["Building Name"])
+
+	# Alphabetize the array before returning it as JSON to the client.
+	alphaBuildings = sorted(buildingArr)
+	return jsonify(buildings=alphaBuildings)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
--- static/js/empty.js
+++ static/js/empty.js
@@ -1,31 +1,65 @@
-//console.log("Hello world");
-//console.log($('#building option:selected').text())
+// Get the current day of the week and set it as the pre populated option for Day (there are no weekend classes so we set them to Monday).
+$(document).ready( function() {
+  switch (new Date().getDay()) {
+    case 0:
+        day = "Monday";
+        break;
+    case 1:
+        day = "Monday";
+        break;
+    case 2:
+        day = "Tuesday";
+        break;
+    case 3:
+        day = "Wednesday";
+        break;
+    case 4:
+        day = "Thursday";
+        break;
+    case 5:
+        day = "Friday";
+        break;
+    case 6:
+        day = "Monday";
+        break;
+  }  
+  $("#day").val(day);
+
+  // On page load we clear the buildings in the Building selector and reset the Campus selector.  This is to prevent invalid queries and force the GET request to fire on switching options for campus.
+  $('#building').html("<select type='select' required='' name='building' id='building' class='form-control'><option value='Please choose a campus first'>Please choose a campus first</option></select>");
+  $("#campus").val("Please choose a campus");
+});
+
+// Detects a change in the Building selector.  When this fires, it checks to make sure that you have anything but the default option selected.  If you, do it will reenable the submit button.
 $('#building').change(function () {
   if ($('#building option:selected').text() == 'Please choose a campus first') {
     $('#submitQ').prop('disabled', true);
-    //console.log('disabled');
   } else {
     $('#submitQ').prop('disabled', false);
-    //console.log('enabled');
-  }
-  //console.log($('#building option:selected').text())
-});
-$('#campus').change(function () {
-  if ($('#campus option:selected').text() == 'Please choose a campus') {
-    $('#submitQ').prop('disabled', true);
-    //console.log('disabled');
   }
 });
 
+// Detects a change in the campus selector, making sure that you have anything but the default option chosen.  If so it will reenable the submit button.
 $('#campus').change(function () {
+  if ($('#campus option:selected').text() == 'Please choose a campus') {
+    $('#submitQ').prop('disabled', true);
+  }
+});
+
+// Detect a change in the campus selector.  This fires a GET request which gets the buildings for the chosen campus.
+$('#campus').change(function () {
+
+  // Resets the Building selector so that when we change campuses we dont just add to the choices already populated.
   $('#building').html("<select type='select' required='' name='building' id='building' class='form-control'><option value='Please choose a campus first'>Please choose a campus first</option></select>")
   $.get('/_getBuildings', {
     campus: $('#campus option:selected').text()
-  }, function(data){
-	  var numItems = data.buildings.length;
-	  for(items in data.buildings){
+  }, function(responseArr){
+
+	  // Populates the Building selector with each item from the server response.
+	  var numItems = responseArr.buildings.length;
+	  for(items in responseArr.buildings){
 		  var selForm = document.myForm.building
-		  selForm.options[selForm.options.length] = new Option(data.buildings[items], data.buildings[items]);
+		  selForm.options[selForm.options.length] = new Option(responseArr.buildings[items], responseArr.buildings[items]);
 	  }
   })
 });
